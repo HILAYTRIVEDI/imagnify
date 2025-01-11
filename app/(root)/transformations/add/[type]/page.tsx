7c5bdf97b4fd1eb0e18d067f4d016500f23ee84e
HILAYTRIVEDI <52625748+HILAYTRIVEDI@users.noreply.github.com>
2025-01-11 20:09:30 +0530

    Feat: add buttons for the image modification

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -22,12 +22,16 @@ const AddTransformationTypePage = async ({params}:SearchParamProps) => {
         title={`Add ${transformation.title} Transformation`}
         subtitle={transformation.subTitle}
       />
-      <TransformationForm  
-        action='Add' 
-        userId={ user._id }
-        type={ transformation.type as TransformationTypeKey}
-        creditBalance={ user.creditBalance }
+      <section
+        className='mt-10'
+      >
+        <TransformationForm
+          action='Add'
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
         />
+      </section>
     </>
   )
 }
--- components/shared/TransformationForm.tsx
+++ components/shared/TransformationForm.tsx
@@ -4,7 +4,7 @@ import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { Button } from "@/components/ui/button"
-import { defaultValues } from "@/constants"
+import { aspectRatioOptions, defaultValues, transformationTypes } from "@/constants"
 import
     {
         Form,
@@ -16,7 +16,19 @@ import
         FormMessage,
     } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { title } from "process"
+import { CustomField } from "./CustomField"
+import
+    {
+        Select,
+        SelectContent,
+        SelectItem,
+        SelectTrigger,
+        SelectValue,
+    } from "@/components/ui/select"
+import { useState } from "react"
+import { AspectRatioKey } from "@/lib/utils"
+import { config } from "process"
+
 
 export const formSchema = z.object({
     title: z.string().nonempty(),
@@ -26,7 +38,19 @@ export const formSchema = z.object({
     publicId: z.string(),
 })
 
-const TransformationForm = ({ action, data= null  }: TransformationFormProps) => {
+const TransformationForm = ({ action, data= null, userId, type, creditBalance, config=null  }: TransformationFormProps) => {
+
+    const transformation = transformationTypes[type]
+
+    const [image, setImage] = useState(false)
+    const [newTransformation, setNewTransformation] = useState<Transformations | null>(null)
+    const [isSubmitting, setIsSubmitting] = useState(false)
+    const [isTransforming, setIsTransforming] = useState(false)
+    const [transformationConfig, setTransformationConfig] = useState(config)
+
+    const onInputChangeHandler = ( fieldName:string, value:string, type:string, onChange:( (type:string) => void ) ) => {
+
+    }
 
     const initialValues = data && action === "Update" ? {
         title: data?.title,
@@ -47,10 +71,107 @@ const TransformationForm = ({ action, data= null  }: TransformationFormProps) =>
     {
         console.log(values)
     }
+
+    const onTransformHandler = () =>{
+        // pass
+    }
+
+    const onSelectFieldHandler = (value:string , onChange:(value:string)=>void) =>
+    {
+        // Pass
+    }
+
     return (
         <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-
+                <CustomField
+                    control={form.control}
+                    name="title"
+                    formLabel="Image Title"
+                    className="w-full"
+                    render={({ field }) => (
+                        <Input {...field} placeholder="Enter the image title" className="input-field"/>
+                    )}
+                />
+                {
+                    type === 'fill' && (
+                        <CustomField
+                            control={form.control}
+                            name="aspectRatio"
+                            formLabel="Aspect Ratio"
+                            className="w-full"
+                            render={ ({ field }) => (
+                                <Select
+                                    onValueChange={(value) => onSelectFieldHandler(value, field.onChange)}
+                                >
+                                    <SelectTrigger className="select-fill">
+                                        <SelectValue placeholder="Select Size" />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        {
+                                            Object.keys( aspectRatioOptions ).map((key) => (
+                                                <SelectItem key={key} value={key} className="select-item">
+                                                    {aspectRatioOptions[key as AspectRatioKey].label}
+                                                </SelectItem>
+                                            ))
+                                        }
+                                    </SelectContent>
+                                </Select>  
+                            )}
+                        />
+                    )
+                }
+                {
+                    (type === 'remove' || type === 'recolor') && (
+                        <div className="prompt-field">
+                            <CustomField
+                                control={form.control}
+                                name="prompt"
+                                formLabel={ type === 'remove' ? "Remove Background" : "Recolor Image"}
+                                className="w-full"
+                                render={({ field }) => (
+                                    <Input 
+                                        value={field.value}
+                                        onChange={(e) => { onInputChangeHandler('prompt',e.target.value, type, field.onChange) }}
+                                        className="input-field"
+                                    />
+                                )}
+                            />
+                            {  type === 'recolor' && (
+                                <CustomField
+                                    control={form.control}
+                                    name="recolor"
+                                    formLabel="Replacement Color"
+                                    className="w-full"
+                                    render={({ field }) => (
+                                        <Input 
+                                            value={field.value}
+                                            onChange={(e) => { onInputChangeHandler('color',e.target.value, 'recolor', field.onChange) }}
+                                            className="input-field"
+                                        />
+                                    )}
+                                />
+                            )}
+                        </div>
+                    )
+                }
+                <div className="flex flex-col gap-4">
+                    <Button 
+                        type="submit" 
+                        className="submit-button capitalize"
+                        disabled={isSubmitting }
+                        > 
+                        { isSubmitting ? "Submitting" : "Save Image" } 
+                    </Button>
+                    <Button
+                        type="button"
+                        className="submit-button capitalize"
+                        disabled={isTransforming || newTransformation === null}
+                        onClick={onTransformHandler}
+                    >
+                        { isTransforming ? "Transforming" : "Apply Transformation" }
+                    </Button>
+                </div>
             </form>
         </Form>
     )
