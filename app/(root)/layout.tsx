4978f1b360e97d3fc0a36a0b8f2b0b01a0666b1e
HILAYTRIVEDI <52625748+HILAYTRIVEDI@users.noreply.github.com>
2025-01-26 22:21:10 +0530

    Feat: update page component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,19 +1,21 @@
 import MobileNav from '@/components/shared/MobileNav'
-import SideBar from '@/components/shared/SideBar'
-import React from 'react'
-import { Toaster } from "@/components/ui/toaster";
+import Sidebar from '@/components/shared/SideBar'
+import { Toaster } from '@/components/ui/toaster'
 
-const Layout = ({ children }: { children: React.ReactNode } ) => {
+const Layout = ({ children }: { children: React.ReactNode }) =>
+{
   return (
     <main className="root">
-      <SideBar/>
-      <MobileNav/>
+      <Sidebar />
+      <MobileNav />
+
       <div className="root-container">
         <div className="wrapper">
           {children}
         </div>
       </div>
-      <Toaster/>
+
+      <Toaster />
     </main>
   )
 }
